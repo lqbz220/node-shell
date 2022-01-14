698ea68eeb2605ec03598d4fe384135891fdeb2e
Kimberley R <kimberley@example.com>
2022-01-14 15:49:16 -0500

    Made changes.

--- bash.js
+++ bash.js
@@ -1,13 +1,15 @@
-//console.log('hello')
-//console.log(process.stdout)
-//console.dir(process.cwd)
-//process.cwd()
+const pwdFunction = require("./pwd");
+const lsFunction = require("./ls");
+
 process.stdout.write("promot>");
 process.stdin.on("data", (data) => {
-  
-  const cmd = data.toString().trim();
-  const pwd = process.cwd()
+    const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + pwd);
-  process.stdout.write("\nprompt >");
+    if (cmd === "pwd") {
+        process.stdout.write("You typed: " + pwdFunction());
+    } else if (cmd === "ls") {
+        process.stdout.write("You typed: " + lsFunction());
+    }
+
+    process.stdout.write("\nprompt >");
 });
